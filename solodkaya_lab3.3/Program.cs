5255c7a8d5a134390a45160a2831ea1d4da3e0de
Arina <Arina@DESKTOP-H3T0JAQ>
2023-11-12 16:13:08 +0200

    Добавьте файлы проекта.

---
+++ solodkaya_lab3.3/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    Console.WriteLine("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите n:");
+    int n = int.Parse(Console.ReadLine());
+    double s = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        long f = 1;
+        for (int j = 1; j <= i; j++) f *= j;
+        if (i % 2 == 0)
+        {
+            s += Math.Cos(x) / f;
+        }
+        else
+        {
+            s -= Math.Cos(i * x) / f;
+        }
+
+    }
+    Console.WriteLine($"S={s:F2}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
